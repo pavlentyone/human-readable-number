2036118b953551ef8a1c60f1f4752b6966838f75
pavlentyone <pavlentyone@gmail.com>
2022-01-05 13:10:10 +0300

    fix: fixed this tiny things t make my code work properly

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ const units = [
     '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
 ];
 const dozens = [
-    '', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
 ];
 const gitNumbers = [
     'hundred', 'thousand', 'million', 'billion', 'trillion'
 ];
 
 module.exports = function toReadable (number) {
+    if(number === 0)
+        return 'zero';
+
     const unit = number % 10;
     const dozen = Math.trunc(number % 100 / 10);
     const hundred = Math.trunc(number % 1000 / 100);
@@ -20,12 +23,16 @@ module.exports = function toReadable (number) {
         units[dozen * 10 + unit];
     
     if(hundred > 0){
-        result = [unit[hundred], 'hundred', result].join(' ');
+        result = [units[hundred], 'hundred', result].join(' ');
     }
     
     if(thousand > 0){
-        result = [unit[thousand], 'thousand', result].join(' ');
+        result = [units[thousand], 'thousand', result].join(' ');
     }
-    
+
+    if(result[result.length - 1] === ' '){
+        result = result.substring(0, result.length - 1);
+    }
+
     return result;
 }
